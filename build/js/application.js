48fdcc62b79a25d923d820200fb62048276aa5a1
FunnyQ <funnyq@funnyq.com>
2014-08-27 13:49:06 +0800

    build

--- build/js/application.js
+++ build/js/application.js
@@ -1,19 +1,22 @@
 (function() {
-  var courseSlider, indexSlider;
+  // Slider blocks;
+  var activitiesSlider, courseSlider, indexSlider;
 
   indexSlider = $('.index_slider .nivoSlider');
 
   courseSlider = $('.course_slider .nivoSlider');
 
+  activitiesSlider = $('.activities_slider .nivoSlider');
+
   $(window).load(function() {
-    indexSlider.nivoSlider({
+    // using nivoSlider;
+    indexSlider.add(courseSlider).add(activitiesSlider).nivoSlider({
       effect: 'sliceDown',
       animSpeed: 800,
       pauseTime: 5000,
       prevText: '上一張',
       nextText: "下一張"
     });
-    courseSlider.nivoSlider();
   });
 
 }).call(this);
